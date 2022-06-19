a1e110c4cadbae4c2412c102bc840a5c60b57493
elizkhoo731 <elizkhoo731@gmail.com>
2022-06-20 02:14:01 +0800

    20 June, 2.10am - Fix some errors for qr code launcher

--- app/src/main/java/com/example/c19trace/CheckIn/CheckInFragment.java
+++ app/src/main/java/com/example/c19trace/CheckIn/CheckInFragment.java
@@ -23,6 +23,8 @@ import com.example.c19trace.R;
 import com.example.c19trace.databinding.ActivityMainBinding;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -77,45 +79,77 @@ public class CheckInFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                IntentIntegrator integrator = IntentIntegrator.forSupportFragment(CheckInFragment.this);
+//                IntentIntegrator integrator = IntentIntegrator.forSupportFragment(CheckInFragment.this);
+//
+//                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+//                integrator.setPrompt("Scan");
+//                integrator.setCameraId(0);
+//                integrator.setBeepEnabled(true);
+//                integrator.setBarcodeImageEnabled(true);
+//                integrator.setOrientationLocked(true);
+//
+//                integrator.initiateScan();
 
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-                integrator.setPrompt("Scan");
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(true);
-                integrator.setBarcodeImageEnabled(true);
-                integrator.setOrientationLocked(true);
+                ScanOptions options = new ScanOptions();
 
-                integrator.initiateScan();
+                options.setDesiredBarcodeFormats(ScanOptions.ONE_D_CODE_TYPES);
+                options.setPrompt("Scan");
+                options.setCameraId(0);  // Use a specific camera of the device
+                options.setBeepEnabled(true);
+                options.setBarcodeImageEnabled(true);
+                options.setOrientationLocked(true);
+
+                qrLauncher.launch(options);
             }
         });
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+//
+//        if(result != null) {
+//            if(result.getContents() == null) { //not successful
+//                Log.e("Scan*******", "Cancelled scan");
+//                Toast.makeText(getActivity(), "Cancelled scan", Toast.LENGTH_LONG).show();
+//
+//            } else { //successful get the string from the QR code
+//                Log.e("Scan", "Scanned");
+//
+//                String scan_location = result.getContents();
+//                String current_date = java.time.LocalDate.now().toString();
+//
+//                HistoryClass history = new HistoryClass(scan_location, current_date);
+//                mainViewModel.insert(history);
+//
+//                int historyListSize = historyList.size();
+//                historyList.addLast(history);
+//                recyclerView.getAdapter().notifyDataSetChanged();
+//                recyclerView.smoothScrollToPosition(historyListSize);
+//            }
+//        } else {
+//            super.onActivityResult(requestCode, resultCode, data);
+//        }
+//    }
 
-        if(result != null) {
-            if(result.getContents() == null) { //not successful
-                Log.e("Scan*******", "Cancelled scan");
-                Toast.makeText(getActivity(), "Cancelled scan", Toast.LENGTH_LONG).show();
+    private final ActivityResultLauncher<ScanOptions> qrLauncher = registerForActivityResult(new ScanContract(),
+            result -> {
+                if(result.getContents() == null){
+                    Log.e("Scan*******", "Cancelled scan");
+                    Toast.makeText(getActivity(), "Cancelled scan", Toast.LENGTH_LONG).show();
+                } else{
+                    Log.e("Scan", "Scanned");
 
-            } else { //successful get the string from the QR code
-                Log.e("Scan", "Scanned");
+                    String scan_location = result.getContents();
+                    String current_date = java.time.LocalDate.now().toString();
 
-                String scan_location = result.getContents();
-                String current_date = java.time.LocalDate.now().toString();
+                    HistoryClass history = new HistoryClass(scan_location, current_date);
+                    mainViewModel.insert(history);
 
-                HistoryClass history = new HistoryClass(scan_location, current_date);
-                mainViewModel.insert(history);
-
-                int historyListSize = historyList.size();
-                historyList.addLast(history);
-                recyclerView.getAdapter().notifyDataSetChanged();
-                recyclerView.smoothScrollToPosition(historyListSize);
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
+                    int historyListSize = historyList.size();
+                    historyList.addLast(history);
+                    recyclerView.getAdapter().notifyDataSetChanged();
+                    recyclerView.smoothScrollToPosition(historyListSize);
+                }
+            });
 }
